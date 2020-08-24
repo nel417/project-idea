986cc1a5a1cedea020d9f71293de84c7903ad480
nick L <xnel417x@gmail.com>
2020-08-24 17:31:09 -0500

    Played around with basic auth and looping

--- main.go
+++ main.go
@@ -1,21 +1,57 @@
 package main
 
-import "github.com/gofiber/fiber"
+import (
+	"fmt"
+	"log"
+
+	"github.com/gofiber/basicauth"
+	"github.com/gofiber/fiber"
+	"github.com/gofiber/template/html"
+)
 
 func main() {
+	x := make([]string, 3)
+	x[0] = "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/540fe1b392f2f-03-tcx-guy-celebrating-retro-tennis-gear-0812-xl-lg-1529073447.jpg?crop=0.862xw:1.00xh;0.0374xw,0&resize=480:*"
+	x[1] = "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/540fe1b392f2f-03-tcx-guy-celebrating-retro-tennis-gear-0812-xl-lg-1529073447.jpg?crop=0.862xw:1.00xh;0.0374xw,0&resize=480:*"
+	x[2] = "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/540fe1b392f2f-03-tcx-guy-celebrating-retro-tennis-gear-0812-xl-lg-1529073447.jpg?crop=0.862xw:1.00xh;0.0374xw,0&resize=480:*"
 
-	app := fiber.New()
-	app.Static("/", "./public")
+	engine := html.New("./views", ".html")
+
+	app := fiber.New(&fiber.Settings{
+		Views: engine,
+	})
+
+	cfg := basicauth.Config{
+		Users: map[string]string{
+			"Nick":  "Sharks12",
+			"admin": "123456",
+		},
+	}
+	app.Use(basicauth.New(cfg))
+	//Rendered assets
 
 	app.Get("/", func(c *fiber.Ctx) {
-		if err := c.Render("index", fiber.Map{}); err != nil {
+		username := c.Locals("username").(string)
+		password := c.Locals("password").(string)
+		fmt.Println(username, password)
+		if err := c.Render("index", fiber.Map{
+			"Title": "Nick Landreville Studios",
+			"loopy": x,
+		}); err != nil {
 			c.Next(err)
 		}
 	})
 
 	app.Get("/about", func(c *fiber.Ctx) {
-		c.Send("about")
+		// Static fn
+		// c.SendFile("./public/about.html")
+		// Render fn
+		if err := c.Render("about", fiber.Map{}); err != nil {
+			c.Next(err)
+		}
 	})
+	// Static assets ( .css, .js etc )
+	app.Static("/", "./public")
 
-	app.Listen(3000)
+	log.Fatal(app.Listen(3000))
 }
