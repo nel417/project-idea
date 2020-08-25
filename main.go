ca9b69761c3e8ad385815c4c54e23234cf31bdba
nick L <xnel417x@gmail.com>
2020-08-24 20:24:02 -0500

    fixed img width, box sizing, and added footer

--- main.go
+++ main.go
@@ -10,13 +10,13 @@ import (
 )
 
 func main() {
+	// moodboard images
 	x := make([]string, 3)
 	x[0] = "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/540fe1b392f2f-03-tcx-guy-celebrating-retro-tennis-gear-0812-xl-lg-1529073447.jpg?crop=0.862xw:1.00xh;0.0374xw,0&resize=480:*"
 	x[1] = "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/540fe1b392f2f-03-tcx-guy-celebrating-retro-tennis-gear-0812-xl-lg-1529073447.jpg?crop=0.862xw:1.00xh;0.0374xw,0&resize=480:*"
 	x[2] = "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/540fe1b392f2f-03-tcx-guy-celebrating-retro-tennis-gear-0812-xl-lg-1529073447.jpg?crop=0.862xw:1.00xh;0.0374xw,0&resize=480:*"
-
+	//end mooboard images
 	engine := html.New("./views", ".html")
-
 	app := fiber.New(&fiber.Settings{
 		Views: engine,
 	})
@@ -29,7 +29,6 @@ func main() {
 	}
 	app.Use(basicauth.New(cfg))
 	//Rendered assets
-
 	app.Get("/", func(c *fiber.Ctx) {
 		username := c.Locals("username").(string)
 		password := c.Locals("password").(string)
@@ -41,7 +40,6 @@ func main() {
 			c.Next(err)
 		}
 	})
-
 	app.Get("/about", func(c *fiber.Ctx) {
 		// Static fn
 		// c.SendFile("./public/about.html")
@@ -52,6 +50,5 @@ func main() {
 	})
 	// Static assets ( .css, .js etc )
 	app.Static("/", "./public")
-
 	log.Fatal(app.Listen(3000))
 }
