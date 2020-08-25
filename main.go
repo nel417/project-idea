cf19a0899b1d6063fcad8b7aa0d6d6d12d93e9e0
nick L <xnel417x@gmail.com>
2020-08-25 11:50:42 -0500

    worked on fonts and added new moodboard images

--- main.go
+++ main.go
@@ -11,11 +11,12 @@ import (
 
 func main() {
 	//moodboard images
-	x := make([]string, 4)
+	x := make([]string, 5)
 	x[0] = "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/540fe1b392f2f-03-tcx-guy-celebrating-retro-tennis-gear-0812-xl-lg-1529073447.jpg?crop=0.862xw:1.00xh;0.0374xw,0&resize=480:*"
 	x[1] = "https://i.pinimg.com/564x/e1/a3/58/e1a35879d8fc23371df151d3850bce7c.jpg"
 	x[2] = "https://upload.wikimedia.org/wikipedia/en/6/63/Right_Now%2C_Wrong_Then_%28poster%29.jpg"
 	x[3] = "https://asianwiki.com/images/d/da/Right_Now%2C_Wrong_Then-p1.jpg"
+	x[4] = "https://mir-s3-cdn-cf.behance.net/project_modules/1400/8146e165435111.5af41549b7840.jpg"
 	//end mooboard images
 
 	engine := html.New("./views/", ".html")
