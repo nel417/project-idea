f6d95f561468a5448c9b8b2ef5752c11f2d9e112
nick L <xnel417x@gmail.com>
2020-08-25 21:14:22 -0500

    Added moodboard pictures

--- main.go
+++ main.go
@@ -11,12 +11,17 @@ import (
 
 func main() {
 	//moodboard images
-	x := make([]string, 5)
+	x := make([]string, 10)
 	x[0] = "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/540fe1b392f2f-03-tcx-guy-celebrating-retro-tennis-gear-0812-xl-lg-1529073447.jpg?crop=0.862xw:1.00xh;0.0374xw,0&resize=480:*"
 	x[1] = "https://i.pinimg.com/564x/e1/a3/58/e1a35879d8fc23371df151d3850bce7c.jpg"
 	x[2] = "https://upload.wikimedia.org/wikipedia/en/6/63/Right_Now%2C_Wrong_Then_%28poster%29.jpg"
 	x[3] = "https://asianwiki.com/images/d/da/Right_Now%2C_Wrong_Then-p1.jpg"
 	x[4] = "https://mir-s3-cdn-cf.behance.net/project_modules/1400/8146e165435111.5af41549b7840.jpg"
+	x[5] = "https://shop.wilson.com/en-gb/media/catalog/product/cache/146/image/1800x/040ec09b1e35df139433887a97daa66f/5/c/5c260e027a31c428e87b24990e790838dbe88555_Triniti_alt_2.jpg"
+	x[6] = "https://cdn11.bigcommerce.com/s-vq9b9x8mdr/products/154/images/756/calabasas_tennis_longsleeve__28797.1590614602.386.513.jpg?c=2"
+	x[7] = "https://assets.vogue.com/photos/5b4771e676530c3d905c5491/master/w_1600%2Cc_limit/03-tennis.jpg"
+	x[8] = "https://www.heddels.com/wp-content/uploads/2013/09/baldwin-denim-x-suit-supply-13-oz-blue-jeans-collaboration.jpg"
+	x[9] = "https://cnet4.cbsistatic.com/img/d3F8xlb7Y2W-oyk5SpK5lfDuwMQ=/940x0/2019/11/27/1f62650e-4c18-47ab-aece-1b82824c69fc/stevejobs.jpg"
 	//end mooboard images
 
 	engine := html.New("./views/", ".html")
